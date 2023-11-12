dcc2c40b1a127f5f3c76fc82bb8c19005e85e175
Charlie Groves <charliegroves@datavant.com>
2023-11-12 10:54:34 -0500

    Move to current nix2poetry

--- flake.nix
+++ flake.nix
@@ -1,41 +1,26 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=3476a10478587dec90acb14ec6bde0966c545cc0";
-    poetry2nix.url = "github:nix-community/poetry2nix";
-    poetry2nix.inputs.flake-utils.follows = "flake-utils";
-    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    poetry2nix,
-  }:
-    {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          hyperer = prev.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-            # checkGroups Default is [ "dev" ]. We remove that to keep from installing ruff as a nix dep, which is broken currently
-            checkGroups = [];
-          };
-        })
-      ];
-    }
-    // (flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [self.overlay];
-      };
-    in {
-      packages.default = pkgs.hyperer;
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages.default = mkPoetryApplication { projectDir = self; };
 
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [poetry python39 cargo];
-      };
-    }));
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [poetry python39 cargo];
+        };
+      });
 }
